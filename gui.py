886d6f197fca25f3218399dff54dd026d77581f7
HenryNascimentoSilva <henrynascimentosilva@gmail.com>
2023-08-06 16:49:03 -0300

    First Label

--- gui.py
+++ gui.py
@@ -29,7 +29,7 @@ frame_logo = Frame(window, width=850, height=52, bg=colors.co6)
 frame_logo.grid(row=0, column=0, pady=0, padx=0, sticky=NSEW, columnspan=5)
 
 # Logo Image
-app_lg = Image.open('data/logo.png')
+app_lg = Image.open('resources/logo.png')
 app_lg = app_lg.resize((50,50))
 app_lg = ImageTk.PhotoImage(app_lg)
 app_logo = Label(frame_logo, image=app_lg, text="Student Registration System", width=850, compound=LEFT, anchor=NW, font=('Verdana 15'), bg=colors.co6, fg=colors.co1)
@@ -44,9 +44,16 @@ frame_details = Frame(window, width=800, height=100, bg=colors.co1, relief=SOLID
 frame_details.grid(row=1, column=1, pady=1, padx=10, sticky=NSEW)
 
 # Tabel Frame
-frame_details = Frame(window, width=800, height=100, bg=colors.co1, relief=SOLID)
-frame_details.grid(row=3, column=0, pady=0, padx=10, sticky=NSEW, columnspan=5)
+frame_tabel = Frame(window, width=800, height=100, bg=colors.co1, relief=SOLID)
+frame_tabel.grid(row=3, column=0, pady=0, padx=10, sticky=NSEW, columnspan=5)
 
+# Input Field
+
+# Name Field
+l_name = Label(frame_details, text='Name', anchor=NW, font=('Ivy 10'), bg=colors.co1, fg=colors.co4)
+l_name.place(x=4, y=10)
+e_name = Entry(frame_details, width=30, justify='left', relief=SOLID)
+e_name.place(x=7, y=40)
 
 # Window main loop  
 window.mainloop()
